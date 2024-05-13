93e8aa55a920b60b726483200b3ab31e1f250773
Kashyap surani <152683782+Kashyapsurani@users.noreply.github.com>
2024-05-13 13:51:52 +0530

    3.js

--- 3.js
+++ 3.js
@@ -1,13 +1,1 @@
-function printSquare() {
-    var arr = [1, 2, 3, 4];
 
-    var v = arr.forEach((val) => {
-        if (val <=4) {
-            return val * val
-        }
-    })
-    return v
-}
-
-var add = printSquare();
-console.log(add);
